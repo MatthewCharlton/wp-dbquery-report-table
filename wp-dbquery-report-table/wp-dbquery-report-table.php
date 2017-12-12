b384307c785c65072bf60ebcaf2a6f8578a63eb8
MatthewCharlton <33848730+MatthewCharlton@users.noreply.github.com>
2017-12-12 12:46:57 +1100

    Update wp-dbquery-report-table.php

--- wp-dbquery-report-table/wp-dbquery-report-table.php
+++ wp-dbquery-report-table/wp-dbquery-report-table.php
@@ -24,7 +24,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-Copyright 2017 All in IT Solutions
+Copyright 2017 Matt Charlton
 */
 
 defined('ABSPATH') or die('Computer says no!');
